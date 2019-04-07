7f4b46c860921c703a9b6473cc72946b2fcc28da
kevin <00251716@uca.edu.sv>
2019-04-06 22:39:40 -0600

    método doPost implementado

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -3,28 +3,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Guru Registration Form</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Log in</title>
 </head>
 <body>
-<h1>Guru Register Form</h1>
-<form action="guru_register" method="post">
+<h1>Log in</h1>
+<form action="MainServlet" method="post">
 			<table style="with: 50%">
 				<tr>
-					<td>First Name</td>
-					<td><input type="text" name="first_name" /></td>
-				</tr>
-				<tr>
-					<td>Last Name</td>
-					<td><input type="text" name="last_name" /></td>
-				</tr>
-				<tr>
 					<td>UserName</td>
-					<td><input type="text" name="username" /></td>
+					<td><input type="text" name="usr" /></td>
 				</tr>
 					<tr>
 					<td>Password</td>
-					<td><input type="password" name="password" /></td>
+					<td><input type="text" name="pwd" /></td>
 				</tr>
 			</table>
 			<input type="submit" value="Submit" /></form>
--- src/com/ncapas/servlets/MainServlet.java
+++ src/com/ncapas/servlets/MainServlet.java
@@ -1,6 +1,8 @@
 package com.ncapas.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/MainServlet")
 public class MainServlet extends HttpServlet {
+	
+	//Datos quemados
+	private static final String username = "lord_of_darkness88";
+	private static final String password = "1234";
+	
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -34,8 +41,37 @@ public class MainServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		// Primero obtenemos los datos 
+		String input_username = request.getParameter("usr");
+		String input_password = request.getParameter("pwd");
+		
+		PrintWriter out = response.getWriter();
+		
+		//Testing
+		System.out.println("Nombre: " + username);
+		System.out.println("Length expected: " + username.length());
+		System.out.println("Length received: " + input_username.length());
+		System.out.println("Nombre: " + password);
+		System.out.println("Length expected: " + password.length());
+		System.out.println("Length received: " + input_password.length());
+
+		System.out.println(input_username.equals(username));
+		System.out.println(input_password.equals(password));
+		
+		if(input_username.equals(username) && input_password.equals(password)) {
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<h1> <font color=\"green\">Bienvenido, </font> lord_of_darkness88. </h1>");
+			out.println("</body>");
+			out.println("</html>");
+		} else {
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<h1> <font color=\"red\">Error:</font> los datos ingresados no son correctos. </h1>");
+			out.println("</body>");
+			out.println("</html>");
+		}	
 	}
 
 }
